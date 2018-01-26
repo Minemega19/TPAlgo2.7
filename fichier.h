6b6560c1a073fcd6c7a115407de89c1f2769c9ae
Minemega19 <themega19@gmail.com>
2018-01-26 21:22:30 +0100

    Update majeur

--- fichier.h
+++ fichier.h
@@ -1,64 +1,46 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <stdlib.h>
 
 using namespace std;
 
-int CreerFich()
-{
-	string mon_fichier = "tptxt.txt";
-	ifstream fichier(mon_fichier.c_str(), ios::in);
-	if (fichier)
-	{
-		cout << "Fichier ouvert avec succes"<< endl;
-		system("PAUSE");
-		fichier.close();
-	}
-	else 
-		cout << "Impossible d'ouvrir le fichier !" << endl;
-	return 0;
-}
-int LireFich() {
-	return 0;
-}
-int EcrireFich() {
-	ofstream fichier("tptxt.txt", ios::in | ios::trunc);
-	string MaChaine;
-
-	if (fichier)
-	{
-		cout << "Ajouter un mot" << endl;
-		cin >> MaChaine;
-		fichier << MaChaine;
-		fichier.close();
-	}
-	else{
-		cerr << "Impossible d'ouvrir le fichier !" << endl;
-	}
-	return 0;
-}
-void ecrire(){
+void EcrireFichier() {
 	string p;
 	char rep;
-	FILE *f;
+	FILE *fic = fopen("tptxt.txt", "w"); // Ouverture du fichier et lecture ("w")
 	bool reco = true;
-	f=fopen("tptxt.txt","w");
-	if (f==NULL){
-		cout<<"cannot open file"<<endl;
-		return;
+	if (fic == NULL) {
+		cout << "cannot open file" << endl;
+		exit(1);
 	}
-
-	do{
-		cout<<"voulez vous ecrire un mot\n taper o pour oui"<<endl;
-		cin>>rep;
-		if (rep=='o'){
-			cout<<"ecrire le mot a entrer"<<endl;
-			cin>>p;
-			p=p+' ';
-			fwrite(&p,sizeof(string),1,f);
+	do {
+		cout << "voulez vous ecrire un mot\n taper o pour oui" << endl;
+		cin >> rep;
+		if (rep == 'o') {
+			cout << "ecrire le mot a entrer" << endl;
+			cin >> p;
+			p = p + ' ';
+			fwrite(&p, sizeof(string), 1, fic);
 		}
-	}while(rep=='o');
-	fclose(f);
+	} while (rep == 'o');
+	fclose(fic);
 }
-//void 
+
+void LireFichier() {
+	FILE *fic = fopen("tptxt.txt", "r"); // Ouverture du fichier et lecture ("r")
+	int lettre = 0;
+	int i = 0;
+
+	if (fic == NULL) {
+		cout << "Le fichier n a pas pu etre lu"<<endl;
+		system("PAUSE");
+		exit(1); // Le code d erreur si le fichier ne peux pas etre lu
+	}
+	while (lettre != EOF) { // EOF correspond à la fin du fichier
+		lettre = fgetc(fic); // lettre prend le caractere lu par fgetc dans le fichier
+		printf("%c", lettre);
+	}
+	fclose(fic);
+}
 #pragma once
